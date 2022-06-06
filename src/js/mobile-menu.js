24da5f3768ea99b366814fc27a8f9fef435a5481
James Noel <contact@jcnoel.dev>
2022-06-06 03:07:04 -0500

    init repo

---
+++ src/js/mobile-menu.js
@@ -0,0 +1,18 @@
+const menu    = document.querySelector('.menu');
+const links   = document.querySelector('.links');
+const topBar  = document.querySelector('.topBar');
+const midBar  = document.querySelector('.midBar');
+const lowBar  = document.querySelector('.lowBar');
+const logo    = document.querySelector('#logo');
+
+menu.addEventListener('click', () => { 
+    links.classList.toggle('hidden');
+    links.classList.toggle('bg-slate-100'); // for test
+    topBar.classList.toggle('transform');
+    topBar.classList.toggle('translate-y-2'); 
+    topBar.classList.toggle('-rotate-45');
+    midBar.classList.toggle('invisible');
+    lowBar.classList.toggle('transform');
+    lowBar.classList.toggle('-translate-y-2');        
+    lowBar.classList.toggle('rotate-45');
+});
